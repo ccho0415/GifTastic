4d3a141e0ab95ae9220265f5206886da22c2c6c1
ccho0415 <ccho0415@gmail.com>
2016-12-19 01:31:32 -0500

    heroku is picky

--- index.js
+++ index.js
@@ -63,7 +63,7 @@ renderButtons();
 $(".animal").on("click", function(){
   console.log("hi");
   var c = $(this).data("name");
-  var queryURL = "http://api.giphy.com/v1/gifs/search?q="+ c +"&api_key=dc6zaTOxFJmzC&limit=10";
+  var queryURL = "https://api.giphy.com/v1/gifs/search?q="+ c +"&api_key=dc6zaTOxFJmzC&limit=10";
     $.ajax({
       url: queryURL,
       method: "GET"
